7cd91a9d19767df9a9b852044a2532879712d1ab
Xavier Mertens <xavier@rootshell.be>
2013-11-11 11:14:16 +0100

    Miscelaneous fixes

--- post.py
+++ post.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
-#
-# Post a file to a remote site using a local proxy
-# (I'm using this script with BurpSuite)
-#
-import pycurl
-import sys
-import os
-import StringIO
 import argparse
 import ConfigParser
+import os
+import pycurl
+import signal
+import sys
+import StringIO
 from base64 import b64encode
 from optparse import OptionParser
 
@@ -17,9 +14,15 @@ global configFile
 debug = 0
 configFile = './post.conf'
 
+def handler(signal, frame):
+	print "CTRL-C detected!"
+	sys.exit(0)
+
 def main(argv):
 	global dataFile
 	global configFile
+	global requestSoapAction
+	requestSoapAction = ""
 
 	parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
 	parser.add_option('-c', '--config', dest='configFile', type='string', \
@@ -46,6 +49,8 @@ def main(argv):
 	try:
 		config = ConfigParser.ConfigParser()
 		config.read(configFile)
+
+		# Mandatory parameters
 		proxyHost = config.get('proxy', 'host')
 		proxyPort = config.get('proxy', 'port')
 		proxyProtocol = config.get('proxy', 'protocol')
@@ -54,14 +59,20 @@ def main(argv):
 		requestUser = config.get('request', 'username')
 		requestPass = config.get('request', 'password')
 		if debug:
+			print '+++ Using Configuration file:', configFile
 			print "+++ Using proxy: %s:%s" % (proxyHost, proxyPort)
 			print "+++ User-Agent:", requestUA
 			print "+++ Content-Type:", requestType
 	except:
-		print '+++ Cannot open config file', configFile
+		print '+++ Cannot process config file', configFile
 		sys.exit(1)
 
-	if debug: print '+++ Using Configuration file:', configFile
+	# Optional parameters
+	try:
+		requestSoapAction = config.get('request', 'soapaction')
+		if debug: print "+++ Using SoapAction: %s" % requestSoapAction
+	except:
+		pass
 
 	if options.dataFile == None:
 		print "+++ A filename must be provided using the -f switch."
@@ -84,7 +95,6 @@ def main(argv):
 	urlApi = options.URL
 	print '+++ Using target URL:', urlApi
 		
-
 	c = pycurl.Curl()
 	c.setopt(c.PROXY, proxyProtocol + '://' + proxyHost + ':' + proxyPort)
 	c.setopt(c.URL, urlApi)
@@ -95,15 +105,29 @@ def main(argv):
 
 	# Add authentication details
 	c.setopt(pycurl.USERPWD, "%s:%s" % (requestUser, requestPass))
-	c.setopt(pycurl.HTTPHEADER, ["Content-Type: %s" % requestType])
+
+	# Add custom headers
+	headers = []
+	if requestType:
+		headers.append("Content-Type: %s" % requestType)
+	if requestSoapAction:
+		headers.append("SOAPAction: %s" % requestSoapAction)
+
+	c.setopt(pycurl.HTTPHEADER, headers)
+
 	c.setopt(pycurl.POSTFIELDS, f.read())
 	b = StringIO.StringIO()
 	c.setopt(pycurl.WRITEFUNCTION, b.write)
-	c.perform()
-	print "+++ Results:"
-	print b.getvalue()
+	try:
+		c.perform()
+		print "+++ Results:"
+		print b.getvalue()
+	except pycurl.error, e:
+		errorCode, errorText = e.args
+		print "+++ ERROR: Code: %s, Text: %s" % (errorCode, errorText)
 
 if __name__ == '__main__':
+	signal.signal(signal.SIGINT, handler)
 	main(sys.argv[1:])
 	sys.exit()
 
