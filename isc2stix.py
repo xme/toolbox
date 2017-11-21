4dd0068ea917aa7f0abce7d652da31189a6d1d9b
Xavier Mertens <xavier@rootshell.be>
2017-11-21 21:03:15 +0100

    Added condition to 'ip' indicator

--- isc2stix.py
+++ isc2stix.py
@@ -46,6 +46,7 @@ def main():
         ip = Address()
         ip.address_value = "%d.%d.%d.%d" % (int(bytes[0]), int(bytes[1]), int(bytes[2]) , int(bytes[3]))
         ip.category = 'ipv4-addr'
+        ip.condition = 'Equals'
         indicator.add_observable(ip)
 
         package.add_indicator(indicator)
